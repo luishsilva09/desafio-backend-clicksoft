231ec9965d3f0c13e372482bde83212badde5aa4
Luís Henrique <luishsilva09@gmail.com>
2023-08-03 17:23:17 -0300

    feat: validate new student data

--- app/Controllers/Http/StudentsController.ts
+++ app/Controllers/Http/StudentsController.ts
@@ -1,8 +1,30 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Student from 'App/Models/Student'
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
 
 export default class StudentsController {
   public async store({ request, response }: HttpContextContract) {
+    //validar dados
+    const newStudent = schema.create({
+      name: schema.string({}, [rules.required()]),
+      email: schema.string({}, [
+        rules.email(),
+        rules.unique({ table: 'students', column: 'email' }),
+      ]),
+      registration: schema.string({}, [
+        rules.unique({ table: 'students', column: 'registration' }),
+      ]),
+      birthDate: schema.string(),
+    })
+
+    const playload = await request.validate({
+      schema: newStudent,
+      messages: {
+        required: 'Campo {{ field }} é obrigatorio',
+        unique: 'Verificar dados',
+      },
+    })
+
     const body = request.body()
 
     const student = await Student.create(body)
