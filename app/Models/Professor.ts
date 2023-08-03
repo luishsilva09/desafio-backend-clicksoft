e9e652f54998011d56673a5a786741026edfbeb4
Luís Henrique <luishsilva09@gmail.com>
2023-08-03 20:28:08 -0300

    feat: professor model and migration

---
+++ app/Models/Professor.ts
@@ -0,0 +1,29 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column, hasMany, HasMany } from '@ioc:Adonis/Lucid/Orm'
+import Classroom from './Classroom'
+
+export default class Professor extends BaseModel {
+  @hasMany(() => Classroom)
+  public classroom: HasMany<typeof Classroom>
+
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public name: string
+
+  @column({})
+  public email: string
+
+  @column()
+  public registration: string
+
+  @column()
+  public birthDate: string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
---
+++ database/migrations/1691094444621_professors.ts
@@ -0,0 +1,25 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'professors'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('name').notNullable()
+      table.string('email').unique().notNullable()
+      table.string('registration').unique().notNullable()
+      table.string('birth_date').notNullable()
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
