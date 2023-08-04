a1a3fb3f1c07870c0f3f6f6da0dc16003279a762
Luís Henrique <luishsilva09@gmail.com>
2023-08-04 19:40:50 -0300

    feat: mid table

---
+++ database/migrations/1691185064523_class_students.ts
@@ -0,0 +1,24 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'class_students'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('student_id').unsigned().references('students.id')
+      table.integer('classroom_id').unsigned().references('classrooms.id')
+      table.unique(['student_id', 'classroom_id'])
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
