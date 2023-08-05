a2567fdd854bb24fa324d8d1f6d1150b8010da33
Luís Henrique <luishsilva09@gmail.com>
2023-08-05 17:13:26 -0300

    fix: add necessarys id on model

--- app/Models/ClassStudent.ts
+++ app/Models/ClassStudent.ts
@@ -1,9 +1,15 @@
 import { DateTime } from 'luxon'
 import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
 
-export default class ClassStuent extends BaseModel {
+export default class ClassStudent extends BaseModel {
   @column({ isPrimary: true })
   public id: number
+
+  @column()
+  public studentId: number
+
+  @column()
+  public classroomId: number
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
