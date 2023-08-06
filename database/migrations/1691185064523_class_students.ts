a59996efefa3770082dfd698fc5bdaacdcb13182
Luís Henrique <luishsilva09@gmail.com>
2023-08-06 15:55:32 -0300

    fix: to delete in cascade

--- database/migrations/1691185064523_class_students.ts
+++ database/migrations/1691185064523_class_students.ts
@@ -6,8 +6,8 @@ export default class extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.integer('student_id').unsigned().references('students.id')
-      table.integer('classroom_id').unsigned().references('classrooms.id')
+      table.integer('student_id').unsigned().references('students.id').onDelete('CASCADE')
+      table.integer('classroom_id').unsigned().references('classrooms.id').onDelete('CASCADE')
 
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
