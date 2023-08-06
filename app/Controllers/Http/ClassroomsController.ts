e5dd7e8c1e7d9e23ce10e321e2756308dcaefeda
Luís Henrique <luishsilva09@gmail.com>
2023-08-06 15:55:46 -0300

    fix: refactor

--- app/Controllers/Http/ClassroomsController.ts
+++ app/Controllers/Http/ClassroomsController.ts
@@ -27,7 +27,7 @@ export default class ClassroomsController {
     //buscar pelo id do professor
     const professorData = await Professor.findByOrFail('registration', body.professorRegistration)
 
-    const student = await Classroom.create({
+    const classroom = await Classroom.create({
       professorId: professorData.id,
       roomNumber: body.roomNumber,
       studentCapacity: body.studentCapacity,
@@ -37,12 +37,12 @@ export default class ClassroomsController {
     response.status(201)
     return {
       message: 'Sala criado com sucesso',
-      data: student,
+      data: classroom,
     }
   }
 
   //deletar sala
-  public async destroy({ params, request }: HttpContextContract) {
+  public async destroy({ params, request, response }: HttpContextContract) {
     const roomNumber = params.roomNumber
     const body = request.body()
 
@@ -52,18 +52,16 @@ export default class ClassroomsController {
     const roomData = await Classroom.findByOrFail('roomNumber', roomNumber)
 
     //verificar se o professor foi quem criou a sala
-    if (profData.id === roomData.professorId) {
-      await roomData.delete()
-      return {
-        data: 'Deletado com sucesso',
-      }
-    }
+    if (!(await this.validWhoCreateClass(profData.id, roomData.professorId)))
+      return response.status(401).send('Verifique os dados')
+
+    await roomData.delete()
     return {
-      data: 'Verificar dados',
+      data: 'Deletado com sucesso',
     }
   }
   //atualizar dados
-  public async update({ params, request }: HttpContextContract) {
+  public async update({ params, request, response }: HttpContextContract) {
     const body = request.body()
 
     //busca dados da sala
@@ -73,11 +71,8 @@ export default class ClassroomsController {
     const profData = await Professor.findByOrFail('registration', body.profRegistration)
 
     //verificar se o professor foi quem criou a sala
-    if (profData.id !== classroom.professorId) {
-      return {
-        data: 'verifique os dados',
-      }
-    }
+    if (!(await this.validWhoCreateClass(profData.id, classroom.professorId)))
+      return response.status(401).send('Verifique os dados')
 
     //faz a atualização dos dados
     classroom.studentCapacity = body.studentCapacity
@@ -89,6 +84,7 @@ export default class ClassroomsController {
       data: 'Dados atualizados',
     }
   }
+  //buscar por numero da sala
   public async show({ params }: HttpContextContract) {
     const roomNumber = params.roomNumber
 
@@ -98,7 +94,7 @@ export default class ClassroomsController {
       data: classroom,
     }
   }
-
+  // adicionar aluno na sala
   public async addStudent({ params, request, response }: HttpContextContract) {
     const roomNumber = params.roomNumber
     const body = request.body()
@@ -120,9 +116,9 @@ export default class ClassroomsController {
     const studentData = await Student.findByOrFail('registration', body.studentRegistration)
 
     //validar apenas para professor que criou a sala
-    if (roomData.professorId !== profData.id) {
+    if (!(await this.validWhoCreateClass(profData.id, roomData.professorId)))
       return response.status(401).send('Verifique os dados')
-    }
+
     //verificar se  aluno já está na sala
     const existOnClass = await ClassStudent.query()
       .where('student_id', studentData.id)
@@ -152,7 +148,7 @@ export default class ClassroomsController {
       data: 'Aluno adicionado',
     }
   }
-
+  //remover aluno da sala
   public async removeStudent({ params, request, response }: HttpContextContract) {
     const roomNumber = params.roomNumber
     const body = request.body()
@@ -173,9 +169,9 @@ export default class ClassroomsController {
     const studentData = await Student.findByOrFail('registration', body.studentRegistration)
 
     //validar apenas para professor que criou a sala
-    if (roomData.professorId !== profData.id) {
+    if (!(await this.validWhoCreateClass(profData.id, roomData.professorId)))
       return response.status(401).send('Verifique os dados')
-    }
+
     //verificar se  aluno está na sala e deletando
     const existOnClass = await ClassStudent.query()
       .where('student_id', studentData.id)
@@ -200,27 +196,33 @@ export default class ClassroomsController {
       existOnClass,
     })
   }
-
+  //buscar por todos alunos na sala
   public async allStudentClass({ params, request, response }: HttpContextContract) {
     const roomNumber = params.roomNumber
     const body = request.body()
 
     const profData = await Professor.findByOrFail('registration', body.professorRegistration)
 
-    const roomData = await Classroom.query().where('roomNumber', roomNumber)
+    const roomData = await Classroom.findByOrFail('roomNumber', roomNumber)
     //validar apenas para professor que criou a sala
-    if (roomData[0].professorId !== profData.id) {
+    if (!(await this.validWhoCreateClass(profData.id, roomData.professorId)))
       return response.status(401).send('Verifique os dados')
-    }
+
     const studentsList = await Student.query()
       .join('class_students', 'students.id', 'class_students.student_id')
-      .where('class_students.classroom_id', roomData[0].id)
+      .where('class_students.classroom_id', roomData.id)
       .select('name', 'registration', 'email')
 
     return {
       data: 'Lista de alunos',
 
       studentsList,
+    }
+  }
+
+  private async validWhoCreateClass(professorId: number, classProfessorId: number) {
+    if (professorId === classProfessorId) {
+      return true
     }
   }
 }
